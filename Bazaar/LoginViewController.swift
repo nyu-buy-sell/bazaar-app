ced56985d36ce93795a2589716a6137fe0e986c2
Bhavig Pointi <p.bhavig@gmail.com>
2021-11-27 12:08:51 -0500

    login screen

---
+++ Bazaar/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Bazaar
+//
+//  Created by Bhavig Pointi on 11/20/21.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func onRegister(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+
+            }
+        }
+    }
+    
+
+}
+
