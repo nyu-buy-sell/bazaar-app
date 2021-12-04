50d49d235c9ffd10fcbeced015d3d550337ca651
RitaH0 <hanyiran2002@gmail.com>
2021-12-03 23:08:58 -0500

    fixed merge conflict on post screen

--- Bazaar/SceneDelegate.swift
+++ Bazaar/SceneDelegate.swift
@@ -24,6 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
             window?.rootViewController = feedNavigationController
             
+
         }
     }
 
